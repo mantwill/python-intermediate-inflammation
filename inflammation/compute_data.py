d52851ef48670e39dbf7fcdaafa2a6a0a68aa2c4
dansteiert <daniel.steiert@charite.de>
2026-07-01 12:44:48 +0200

    update

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -8,24 +8,74 @@ import numpy as np
 from inflammation import models, views
 
 
-def analyse_data(data_dir):
+# def analyse_data(data_dir):
+#     """Calculates the standard deviation by day between datasets.
+
+#     Gets all the inflammation data from CSV files within a directory,
+#     works out the mean inflammation value for each day across all datasets,
+#     then plots the graphs of standard deviation of these means."""
+#     data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+#     if len(data_file_paths) == 0:
+#         raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+#     data = map(models.load_csv, data_file_paths)
+
+
+#     means_by_day = map(models.daily_mean, data)
+#     means_by_day_matrix = np.stack(list(means_by_day))
+
+#     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+
+#     graph_data = {
+#         'standard deviation by day': daily_standard_deviation,
+#     }
+#     views.visualize(graph_data)
+
+class CSVDataSource()
+    def __init__(data_dir):
+        self.data_dir = data_dir
+
+    def load_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+        return map(models.load_csv, data_file_paths)
+
+class CSVDataSource()
+    def __init__(data_dir):
+        self.data_dir = data_dir
+    def load_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.json'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data JSON files found in path {data_dir}")
+        return map(models.load_json, data_file_paths)
+
+
+
+
+def gen_standard_deviation(data):
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
+
+    
+def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
+    data = data_source.load_data()
 
 
-    means_by_day = map(models.daily_mean, data)
-    means_by_day_matrix = np.stack(list(means_by_day))
+    return gen_standard_deviation(data)
 
-    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
-
-    graph_data = {
-        'standard deviation by day': daily_standard_deviation,
+    
+def plot_data(data, name='standard deviation by day'):
+        graph_data = {
+        name: gen_standard_deviation(data),
     }
     views.visualize(graph_data)
+data_source = CSVDataSource(os.path.dirname(infiles[0]))
+analyse_data(data_source)
